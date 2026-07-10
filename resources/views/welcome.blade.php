d9fc104a9957b4fc091095d6e27e0cb25e16bbaa
Youssef Abd Elhaleim <your_email@example.com>
2026-07-11 02:33:16 +0300

    addddd2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,7 @@
         <!-- Google Fonts Direct Loading -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900&family=Tajawal:wght@300;400;500;700;800;900&family=IBM+Plex+Mono:wght@400;500;600&display=swap" rel="stylesheet">
+        <link href="{{ 'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900&family=Tajawal:wght@300;400;500;700;800;900&family=IBM+Plex+Mono:wght@400;500;600&display=swap' }}" rel="stylesheet">
         
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
